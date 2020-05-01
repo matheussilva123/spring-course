bd9af2b29f30a9dace2d648940a1f861ba8849a0
Matheus Dias <matheus.dias@ifood.com.br>
2020-05-01 16:19:02 -0300

    testando REST

--- src/main/java/com/matheusdias/cursospring/CursospringApplication.java
+++ src/main/java/com/matheusdias/cursospring/CursospringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursospringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursospringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CursospringApplication.class, args);
+    }
 
 }
