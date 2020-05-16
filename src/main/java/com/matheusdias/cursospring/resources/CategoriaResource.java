3c3235c55979817f5ea2e73b6edaccca10808a1d
Matheus Dias <matheus.dias@ifood.com.br>
2020-05-16 17:00:12 -0300

    list apenas categoria

--- src/main/java/com/matheusdias/cursospring/resources/CategoriaResource.java
+++ src/main/java/com/matheusdias/cursospring/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.matheusdias.cursospring.resources;
 
 import com.matheusdias.cursospring.domain.Categoria;
+import com.matheusdias.cursospring.dto.CategoriaDTO;
 import com.matheusdias.cursospring.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -46,6 +49,13 @@ public class CategoriaResource {
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity <List<CategoriaDTO>> findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
