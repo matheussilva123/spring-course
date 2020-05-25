4b80cb6393c5a246f12b7aae820c3bacd1b9e83a
Matheus Dias <matheus.dias@ifood.com.br>
2020-05-25 20:35:33 -0300

    busca de produtos por nome e categorias

---
+++ src/main/java/com/matheusdias/cursospring/dto/ProdutoDTO.java
@@ -0,0 +1,48 @@
+package com.matheusdias.cursospring.dto;
+
+import com.matheusdias.cursospring.domain.Produto;
+
+import java.io.Serializable;
+
+public class ProdutoDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+    private Double preco;
+
+    public ProdutoDTO() {
+    }
+
+    public ProdutoDTO(Produto obj) {
+        id = obj.getId();
+        name = obj.getName();
+        preco = obj.getPreco();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+
+}
