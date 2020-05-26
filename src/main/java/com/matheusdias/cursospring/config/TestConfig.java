4d91c1c8e9232da4e56e719488c4a3cd22f365bf
Matheus Dias <matheus.dias@ifood.com.br>
2020-05-26 15:34:22 -0300

    criando profile de teste

---
+++ src/main/java/com/matheusdias/cursospring/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.matheusdias.cursospring.config;
+
+import com.matheusdias.cursospring.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instatiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
